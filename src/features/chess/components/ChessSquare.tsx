d8eac5bb6d35be114d7d695cb53851d39f9af962
Vladimir Stoikov <vspsmoke@gmail.com>
2026-09-12 13:31:00 +0300

    feat(ChessSquare.tsx): add style and coords to a board

--- src/features/chess/components/ChessSquare.tsx
+++ src/features/chess/components/ChessSquare.tsx
@@ -1,15 +1,27 @@
+import styled from 'styled-components';
+
 interface ChessSquareProps {
   isLight: boolean;
   file: string;
   rank: number;
 }
 
+const ChessSquareSt = styled.div<{ $isLight: boolean }>`
+  background: ${props => (props.$isLight ? 'white' : 'black')};
+  color: ${props => (props.$isLight ? 'black' : 'white')};
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  padding: 4px;
+  font-size: 12px;
+`;
+
 const ChessSquare = ({ isLight, file, rank }: ChessSquareProps) => {
   return (
-    <div>
-      {isLight ? 'light' : 'dark'}-{file}
-      {rank}
-    </div>
+    <ChessSquareSt $isLight={isLight}>
+      {rank === 1 && file}
+      {file === 'a' && rank}
+    </ChessSquareSt>
   );
 };
 
