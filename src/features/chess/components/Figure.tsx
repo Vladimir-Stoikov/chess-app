9000c880ee33d7a2c76f95ab1db6f1a0e11124fd
Vladimir Stoikov <vspsmoke@gmail.com>
2026-09-26 11:35:41 +0300

    feat: add transparent chess piece images and render them on the board

--- src/features/chess/components/Figure.tsx
+++ src/features/chess/components/Figure.tsx
@@ -3,14 +3,22 @@ import styled from 'styled-components';
 import type { IFigure } from '../types';
 
 const FigureSt = styled.span`
-  height: 5px;
-  width: 5px;
+  height: 50px;
+  width: 50px;
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
 `;
 
 const Figure = (props: IFigure) => {
+  const imgSrc = `/img/${props.type}_${props.color[0]}.png`;
+
   return (
     <FigureSt>
-      {props.color} {props.type}
+      <img src={imgSrc} alt={`${props.type}_${props.color}`} />
     </FigureSt>
   );
 };
